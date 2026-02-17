5f18aee4a8ffbb1debc086d17a5b606ef89586d7
Charish Yadav <113330333+charish68@users.noreply.github.com>
2026-02-17 15:51:54 +0530

    Converted login test to data-driven approach

--- automation/tests/test_login.py
+++ automation/tests/test_login.py
@@ -1,13 +1,21 @@
+import json
+import pytest
 from automation.pages.login_page import LoginPage
 from automation.config.config import Config
 
 
-def test_login_with_invalid_credentials(driver):
+def load_test_data():
+    with open("automation/test_data/login_data.json") as f:
+        return json.load(f)
+
+
+@pytest.mark.parametrize("data", load_test_data())
+def test_login_with_invalid_credentials(driver, data):
     driver.get(Config.LOGIN_URL)
 
     login = LoginPage(driver)
-    login.enter_email("wrong@test.com")
-    login.enter_password("wrongpassword")
+    login.enter_email(data["email"])
+    login.enter_password(data["password"])
     login.click_login()
 
     assert "Your email or password is incorrect!" in driver.page_source
