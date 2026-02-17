7ba95c93507700746756b0079d11133625963992
Charish Yadav <113330333+charish68@users.noreply.github.com>
2026-02-17 16:15:30 +0530

    Update test_homepage.py

--- automation/tests/test_homepage.py
+++ automation/tests/test_homepage.py
@@ -1,13 +1,6 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-from automation.pages.home_page import HomePage
+from automation.config.config import Config
 
-def test_homepage_title():
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-    driver.get("https://automationexercise.com")
 
-    home = HomePage(driver)
-    assert "Automation Exercise" in home.get_title()
-
-    driver.quit()
+def test_homepage_title(driver):
+    driver.get(Config.BASE_URL)
+    assert "Automation" in driver.title
