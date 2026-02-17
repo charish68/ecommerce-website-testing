d067f265dc23dafd9e82c4e2901ef805428b5e9c
Charish Yadav <113330333+charish68@users.noreply.github.com>
2026-02-17 16:12:04 +0530

    Fixed missing WebDriverWait import

--- automation/utils/wait_utils.py
+++ automation/utils/wait_utils.py
@@ -1,7 +1,13 @@
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from automation.config.config import Config
+
 
 class WaitUtils:
 
     def __init__(self, driver, timeout=Config.TIMEOUT):
         self.driver = driver
         self.wait = WebDriverWait(driver, timeout)
+
+    def wait_for_element(self, locator):
+        return self.wait.until(EC.visibility_of_element_located(locator))
