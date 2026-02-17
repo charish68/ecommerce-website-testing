f6c31904aaca6f0791c1a79d5fdb885122a62dd6
Charish Yadav <113330333+charish68@users.noreply.github.com>
2026-02-17 15:45:48 +0530

    Integrated timeout from config

--- automation/utils/wait_utils.py
+++ automation/utils/wait_utils.py
@@ -1,12 +1,7 @@
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-
+from automation.config.config import Config
 
 class WaitUtils:
 
-    def __init__(self, driver, timeout=10):
+    def __init__(self, driver, timeout=Config.TIMEOUT):
         self.driver = driver
         self.wait = WebDriverWait(driver, timeout)
-
-    def wait_for_element(self, locator):
-        return self.wait.until(EC.visibility_of_element_located(locator))
